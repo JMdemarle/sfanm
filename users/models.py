0a17b24fbfc98d018eeb448cea39e4aa1d42c47f
Votre Nom <jean-michel@demarle.net>
2020-07-29 21:38:24 +0200

    modifié :         users/models.py

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.contrib.auth.base_user import BaseUserManager
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import AbstractUser
-from phonenumber_field.modelfields import PhoneNumberField
+#from phonenumber_field.modelfields import PhoneNumberField
 
 
 
